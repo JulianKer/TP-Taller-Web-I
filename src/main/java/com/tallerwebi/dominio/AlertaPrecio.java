948a9085aed9f799c9265f0d913cc8c5642f5933
GastonEE01 <gestevez@alumno.unlam.edu.ar>
2024-09-16 11:40:38 -0300

    Completando las clases y relaciones AlertaPrecio y Transaccion

---
+++ src/main/java/com/tallerwebi/dominio/AlertaPrecio.java
@@ -0,0 +1,63 @@
+package com.tallerwebi.dominio;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+    @Entity
+    public class AlertaPrecio {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        public void setCriptomoneda(Criptomoneda criptomoneda) {
+            this.criptomoneda = criptomoneda;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+
+        public void setUsuario(Usuario usuario) {
+            this.usuario = usuario;
+        }
+
+        public Usuario getUsuario() {
+            return usuario;
+        }
+
+        public Criptomoneda getCriptomoneda() {
+            return criptomoneda;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setPrecioAlerta(Double precioAlerta) {
+            this.precioAlerta = precioAlerta;
+        }
+
+        public Double getPrecioAlerta() {
+            return precioAlerta;
+        }
+
+
+        @ManyToOne
+        private Usuario usuario;
+
+        @ManyToOne
+        private Criptomoneda criptomoneda;
+
+        private Double precioAlerta;
+
+        public boolean verificarAlerta(Double precioActual) {
+            return precioActual <= this.precioAlerta;
+        }
+    }
+
---
+++ src/main/java/com/tallerwebi/dominio/Transaccion.java
@@ -0,0 +1,66 @@
+package com.tallerwebi.dominio;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Transaccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    @ManyToOne
+    private Criptomoneda criptomoneda;
+
+    private Double monto;
+    private String tipo; // Compra o Venta
+
+    // Lógica para realizar la transacción
+
+    public Criptomoneda getCriptomoneda() {
+        return criptomoneda;
+    }
+
+    public void setCriptomoneda(Criptomoneda criptomoneda) {
+        this.criptomoneda = criptomoneda;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
