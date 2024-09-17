99effc17da7009100beda2781d949b4056f2c35b
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-09-17 11:11:32 -0300

    agrego test de password en controladorRegistroTest

--- src/main/java/com/tallerwebi/presentacion/ControladorRegistro.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorRegistro.java
@@ -7,12 +7,25 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ControladorRegistro {
 
-    public ModelAndView registrar(String email) {
+    public ModelAndView registrar(String email, String pass1, String pass2) {
+        ModelMap modelo = new ModelMap();
+
+
         if (email.isEmpty()){
-            ModelMap modelo = new ModelMap();
             modelo.put("error", "El email es obligatorio");
             return new ModelAndView("registro", modelo);
         }
+
+        if (pass1.isEmpty() || pass2.isEmpty()){
+            modelo.put("error", "La/s contraseñas estan vacias.");
+            return new ModelAndView("registro", modelo);
+        }
+
+        if (!pass1.equals(pass2)){
+            modelo.put("error", "Las contraseñas no coinciden.");
+            return new ModelAndView("registro", modelo);
+        }
+
         return new ModelAndView("redirect:/login");
     }
 }
--- src/test/java/com/tallerwebi/presentacion/ControladorRegistroTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorRegistroTest.java
@@ -16,7 +16,10 @@ public class ControladorRegistroTest {
         givenNoExisteUsuario();
 
         //ejecucion --> when
-        ModelAndView mav = whenRegistroUsuario("julian@gmail.com");
+        String pass1 = "11";
+        String pass2 = "11";
+        String email = "julian@gmail.com";
+        ModelAndView mav = whenRegistroUsuario(email, pass1, pass2);
 
         //comprobacion --> then
         thenElRegistroesExitoso(mav);
@@ -26,8 +29,8 @@ public class ControladorRegistroTest {
        assertThat(mav.getViewName(), equalToIgnoringCase("redirect:/login"));
     }
 
-    private ModelAndView whenRegistroUsuario(String email) {
-        ModelAndView mav = controladorRegistro.registrar(email);
+    private ModelAndView whenRegistroUsuario(String email, String pass1, String pass2) {
+        ModelAndView mav = controladorRegistro.registrar(email, pass1, pass2);
         return mav;
     }
 
@@ -40,16 +43,19 @@ public class ControladorRegistroTest {
         givenNoExisteUsuario();
 
         //ejecucion --> when
+        String pass1 = "11";
+        String pass2 = "11";
         String emailVacio = "";
-        ModelAndView mav = whenRegistroUsuario(emailVacio);
+        ModelAndView mav = whenRegistroUsuario(emailVacio, pass1, pass2);
 
         //comprobacion --> then
-        thenElRegistroFalla(mav);
+        String msjError = "El email es obligatorio";
+        thenElRegistroFalla(mav, msjError);
     }
 
-    private void thenElRegistroFalla(ModelAndView mav) {
+    private void thenElRegistroFalla(ModelAndView mav, String msjError) {
         assertThat(mav.getViewName(), equalToIgnoringCase("registro"));
-        assertThat(mav.getModel().get("error").toString(), equalToIgnoringCase("El email es obligatorio"));
+        assertThat(mav.getModel().get("error").toString(), equalToIgnoringCase(msjError));
     }
 
     @Test
@@ -57,10 +63,38 @@ public class ControladorRegistroTest {
         //HACER DE TAREA
         //deberiamos cambiar el then, que reciba en total 3 parametros mas (ModelAnView mav, String viewNameEsperado, String claveEsperada, String msjModelEsperado)
         //porque ese then lo vyo a usar en muchos test y no todos verifican el mismo error, al ser en variables, le pasamos lo que queremos que verifique en el test especifico.
+
+        //preparacion --> given
+        givenNoExisteUsuario();
+
+        //ejecucion --> when
+        String pass1 = "";
+        String pass2 = "";
+        String email = "julian@gmail.com";
+
+        ModelAndView mav = whenRegistroUsuario(email, pass1, pass2);
+        String msjError = "La/s contraseñas estan vacias.";
+
+        //comprobacion --> then
+        thenElRegistroFalla(mav, msjError);
     }
 
     @Test
     public void siAlRepetirLasPasswordsSonDistintasElRegistroFalla() {
         //HACER DE TAREA
+
+        //preparacion --> given
+        givenNoExisteUsuario();
+
+        //ejecucion --> when
+        String pass1 = "123";
+        String pass2 = "456";
+        String email = "julian@gmail.com";
+
+        ModelAndView mav = whenRegistroUsuario(email, pass1, pass2);
+        String msjError = "Las contraseñas no coinciden.";
+
+        //comprobacion --> then
+        thenElRegistroFalla(mav, msjError);
     }
 }
