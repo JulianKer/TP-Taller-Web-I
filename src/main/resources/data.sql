0e9468e95d79e8e52df968387e3dbd6bf528c2b0
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-10-09 10:17:49 -0300

    Refactor mensaje de exito/error en suscripcion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
-INSERT INTO Usuario(email, password, rol, activo, nombre, apellido, telefono, fechaNacimiento, saldo) VALUES( 'german@gmail.com', 'german', 'ADMIN', true, 'german', 'schmuker', 12345678, '2004-10-13', 1500.0);
+INSERT INTO Usuario(email, password, rol, activo, nombre, apellido, telefono, fechaNacimiento, saldo) VALUES( 'german@gmail.com', 'german', 'ADMIN', false, 'german', 'schmuker', 12345678, '2004-10-13', 1500.0);
 INSERT INTO Criptomoneda(nombre, precioActual, imagen) VALUES
                                                    ('bitcoin',100.0, 'Bitcoin.webp'), --sacar este punto y coma
                                                    ('ethereum', 100.0, 'Ethereum.webp'),
