928ab512c421907aa3a18af74657b16979a15867
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-10-25 16:14:59 -0300

    refactor de como se muestran las demas transacciones en el historial

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
 INSERT INTO Usuario(email, password, rol, activo, nombre, apellido, telefono, fechaNacimiento, saldo, estaBloqueado) VALUES( 'tomi@gmail.com', 'tomi', 'CLIENTE', false, 'tomi', 'nania', 12345678, '2004-10-13', 100000.0, false);
-INSERT INTO Usuario(email, password, rol, activo, nombre, apellido, telefono, fechaNacimiento, saldo, estaBloqueado) VALUES( 'julianschker@gmail.com', 'julian', 'CLIENTE', false, 'julian', 'schmuker', 12345678, '2004-10-13', 204240.0, false);
-INSERT INTO Usuario(email, password, rol, activo, nombre, apellido, telefono, fechaNacimiento, saldo, estaBloqueado) VALUES( 'german@gmail.com', 'german', 'CLIENTE', false, 'german', 'schmuker', 12345678, '2004-10-13', 1500.0, false);
+INSERT INTO Usuario(email, password, rol, activo, nombre, apellido, telefono, fechaNacimiento, saldo, estaBloqueado) VALUES( 'julianschker@gmail.com', 'julian', 'CLIENTE', false, 'julian', 'schmuker', 12345678, '2004-10-13', 240000.0, false);
+INSERT INTO Usuario(email, password, rol, activo, nombre, apellido, telefono, fechaNacimiento, saldo, estaBloqueado) VALUES( 'german@gmail.com', 'german', 'CLIENTE', false, 'german', 'schmuker', 12345678, '2004-10-13', 350000.0, false);
 INSERT INTO Usuario(email, password, rol, activo, nombre, apellido, telefono, fechaNacimiento, saldo, estaBloqueado) VALUES( 'agustincardelli@gmail.com', 'prubaagus', 'CLIENTE', false, 'agustin', 'cardelli', 12345679, '2003-10-4', 300000.0, false);
 INSERT INTO Usuario(email, password, rol, activo, nombre, apellido, telefono, fechaNacimiento, saldo, estaBloqueado) VALUES( 'germanschmuker@gmail.com', 'pruebaGerman', 'CLIENTE', false, 'german', 'schmuker', 12345675, '2004-10-13', 300000.0, false);
 INSERT INTO Criptomoneda(nombre, precioActual, imagen, habilitada) VALUES
